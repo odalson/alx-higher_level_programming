8fda7bad5a307d1f06196de774645eede560d551
odalson <melvin4gups@gmail.com>
2024-01-04 18:13:06 -0800

    import and modules

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, m):
+    """Match bytecode provided by Holberton School."""
+    from magic_calculation_102 import add, sub
+
+    if a < m:
+        k = add(a, m)
+        for i in range(4, 6):
+            k = add(c, i)
+        return (k)
+    else:
+        return(sub(a, m))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,4 @@
+Python - import & modules
+
+In this project, I will be  importing and using functions and creating modules in Python. I should have  practiced using the builtin function and using command line arguments within Python programs.
+
